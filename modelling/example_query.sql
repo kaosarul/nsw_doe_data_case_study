76edce6846c3db668e8674af62ff26c728121a28
Example User <eUser@example.com>
2023-08-17 12:17:35 +0000

    multi_age_comp n student_attendance tables created

--- modelling/example_query.sql
+++ modelling/example_query.sql
@@ -1,5 +1,1 @@
-SELECT table_name 
-FROM information_schema.tables 
-WHERE table_schema = 'main'
-AND table_type = 'VIEW'
-AND table_name = 'example_model';
+CREATE TABLE multi_age_composite_dataset AS SELECT * FROM read_csv_auto('./data/multi_age_composite_classes.csv');
