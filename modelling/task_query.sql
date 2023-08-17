87b7dbd448666ff84281eb1a6e01f5dc1ff50f58
Example User <eUser@example.com>
2023-08-17 16:04:17 +0000

    exploring tables data

--- modelling/task_query.sql
+++ modelling/task_query.sql
@@ -2,5 +2,23 @@ CREATE TABLE multi_age_composite_dataset AS SELECT * FROM read_csv_auto('./data/
 
 CREATE TABLE student_attendance_data AS SELECT * FROM read_csv_auto('./data/attendance_data.csv');
 
-SELECT * from multi_age_composite_dataset;
-SELECT * from multi_age_composite_dataset
+SELECT * from multi_age_composite_unpivoted;
+SELECT * from public_school_nsw_master_dataset;
+
+ALTER TABLE unpivoted_dataset RENAME TO multi_age_composite_unpivoted;
+
+ALTER TABLE multi_age_composite_unpivoted 
+RENAME COLUMN value TO Composite_Value;
+
+ALTER TABLE multi_age_composite_unpivoted 
+ALTER COLUMN School_Code SET DATA TYPE INT;
+
+ALTER TABLE multi_age_composite_unpivoted 
+ALTER COLUMN Year SET DATA TYPE INT;
+
+UPDATE multi_age_composite_unpivoted
+SET Composite_Value = NULL
+WHERE Composite_Value = 'NULL';
+
+ALTER TABLE multi_age_composite_unpivoted
+ALTER COLUMN Composite_Value SET DATA TYPE FLOAT;
