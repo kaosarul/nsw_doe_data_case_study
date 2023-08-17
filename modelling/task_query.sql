4a09f087fe679fb7978e1e06c97342c965029f0a
Example User <eUser@example.com>
2023-08-17 13:57:11 +0000

    Update codes

---
+++ modelling/task_query.sql
@@ -0,0 +1,5 @@
+CREATE TABLE multi_age_composite_dataset AS SELECT * FROM read_csv_auto('./data/multi_age_composite_classes.csv');
+
+CREATE TABLE student_attendance_data AS SELECT * FROM read_csv_auto('./data/attendance_data.csv');
+
+SELECT * from multi_age_composite_dataset
