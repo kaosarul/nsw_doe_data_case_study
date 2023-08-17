b35073837565a6bfa367dad00bdba12052cc5779
Example User <eUser@example.com>
2023-08-17 12:03:08 +0000

    warmup

--- modelling/example_model.sql
+++ modelling/example_model.sql
@@ -1,4 +1,3 @@
--- due to duckdb sqltools 
 create view example_model as
 select *, 'annother_transformation' as annother_transformation
 from public_school_nsw_master_dataset
--- modelling/example_query.sql
+++ modelling/example_query.sql
@@ -1,2 +1,5 @@
-select *
-from public_school_nsw_master_dataset 
+SELECT table_name 
+FROM information_schema.tables 
+WHERE table_schema = 'main'
+AND table_type = 'VIEW'
+AND table_name = 'example_model';
