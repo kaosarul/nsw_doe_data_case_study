c9f288f690ec5edf541c8bdbca83e56ca0980ab1
Example User <eUser@example.com>
2023-08-18 08:02:47 +0000

    Added SQL views for data modelling

--- modelling/task_query.sql
+++ modelling/task_query.sql
@@ -67,6 +67,35 @@ ALTER COLUMN Attendance_pct SET DATA TYPE DOUBLE;
 
 
 
+CREATE VIEW school_master_data AS
+SELECT 
+    School_code, School_name, Street, Town_suburb, Postcode, Phone, 
+    School_Email, Website, Fax, LGA, ASGS_remoteness, AECG_region, Latitude, Longitude
+FROM 
+    public_school_nsw_master_dataset;
+
+CREATE VIEW school_attendance_data AS
+SELECT 
+    School_Code, Year, Attendance_pct
+FROM 
+    student_attendance_unpivoted;
+
+CREATE VIEW school_composite_data AS
+SELECT 
+    School_code, Year, Measure, Composite_Value
+FROM 
+    multi_age_composite_unpivoted;
+
+CREATE VIEW school_characteristics AS
+SELECT 
+    School_code, Level_of_schooling, Selective_school, Opportunity_class, 
+    School_specialty_type, latest_year_enrolment_FTE, Indigenous_pct, LBOTE_pct
+FROM 
+    public_school_nsw_master_dataset;
+
+
+
+
 
 
 
