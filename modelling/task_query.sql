51f22313222d0703e95a6caf620fd62a0dda5dbb
Example User <eUser@example.com>
2023-08-18 14:56:11 +0000

    Tables Aligned

--- modelling/task_query.sql
+++ modelling/task_query.sql
@@ -1,83 +1,123 @@
-CREATE TABLE multi_age_composite_dataset AS SELECT * FROM read_csv_auto('./data/multi_age_composite_classes.csv');
+We would like you to develop a PhD research proposal for the title: 
 
-CREATE TABLE student_attendance_data AS SELECT * FROM read_csv_auto('./data/attendance_data.csv');
+"Deep Transformation of Tabular Categorical Data for Enhanced Predictive Analytics in Health Informatics". 
 
-SELECT * from multi_age_composite_unpivoted;
-SELECT * from public_school_nsw_master_dataset;
+Please find a tentative summary of the project as given below:
 
-ALTER TABLE multi_age_composite_unp RENAME TO multi_age_composite_unpivoted;
+This PhD project will delve into the realm of intelligent tabular data transformation with a focus on categorical data, aiming to enhance the accuracy of predictive analytics within the domain of health informatics. By developing advanced deep learning-based techniques for handling categorical data, the research will address a critical challenge in data preprocessing that significantly influences the quality of predictive models in healthcare. The study seeks to create novel methodologies that automate the transformation process, ensuring the efficient conversion of categorical variables into suitable formats for predictive analysis. Through rigorous experimentation and validation, this research promises to contribute substantially to the improvement of predictive analytics in health informatics, ultimately fostering more precise and reliable decision-making in the medical field.
 
-ALTER TABLE multi_age_composite_unpivoted 
-RENAME COLUMN value TO Composite_Value;
+Please find a sample research proposal attached to this email. Part-time PhD may take 3-6 years. You can pick/propose your timeline accordingly.
 
-ALTER TABLE multi_age_composite_unpivoted 
-ALTER COLUMN School_Code SET DATA TYPE BIGINT;
+I am also attaching two research papers to start the literature review for you.
 
-ALTER TABLE multi_age_composite_unpivoted 
-ALTER COLUMN Year SET DATA TYPE INT;
+Tuition fee will be waived under RTP, and we will seek the approval of it for you. We will also seek for industry collaboration for your PhD project.
 
-UPDATE multi_age_composite_unpivoted
-SET Composite_Value = NULL
-WHERE Composite_Value = 'NULL';
 
-ALTER TABLE multi_age_composite_unpivoted
-ALTER COLUMN Composite_Value SET DATA TYPE FLOAT;
 
-UPDATE public_school_nsw_master_dataset
-SET Phone = REPLACE(Phone, ' ', '');
 
-UPDATE public_school_nsw_master_dataset 
-SET Indigenous_pct = NULL 
-WHERE Indigenous_pct = 'np';
 
-ALTER TABLE public_school_nsw_master_dataset 
-ALTER COLUMN Indigenous_pct SET DATA TYPE DOUBLE;
 
-UPDATE public_school_nsw_master_dataset 
-SET LBOTE_pct = NULL 
-WHERE LBOTE_pct = 'np';
 
-ALTER TABLE public_school_nsw_master_dataset 
-ALTER COLUMN LBOTE_pct SET DATA TYPE DOUBLE;
 
-UPDATE public_school_nsw_master_dataset
+
+
+
+
+
+
+CREATE TABLE MULTI_AGE_COMPOSITE_DATASET AS SELECT * FROM READ_CSV_AUTO('./DATA/MULTI_AGE_COMPOSITE_CLASSES.CSV');
+
+CREATE TABLE STUDENT_ATTENDANCE_DATA AS SELECT * FROM READ_CSV_AUTO('./DATA/ATTENDANCE_DATA.CSV');
+
+SELECT * FROM MULTI_AGE_COMPOSITE_UNPIVOTED;
+SELECT * FROM PUBLIC_SCHOOL_NSW_MASTER_DATASET;
+
+ALTER TABLE MULTI_AGE_COMPOSITE_UNP RENAME TO MULTI_AGE_COMPOSITE_UNPIVOTED;
+
+ALTER TABLE MULTI_AGE_COMPOSITE_UNPIVOTED 
+RENAME COLUMN VALUE TO COMPOSITE_VALUE;
+
+ALTER TABLE MULTI_AGE_COMPOSITE_UNPIVOTED 
+ALTER COLUMN SCHOOL_CODE SET DATA TYPE BIGINT;
+
+ALTER TABLE MULTI_AGE_COMPOSITE_UNPIVOTED 
+ALTER COLUMN YEAR SET DATA TYPE INT;
+
+UPDATE MULTI_AGE_COMPOSITE_UNPIVOTED
+SET COMPOSITE_VALUE = NULL
+WHERE COMPOSITE_VALUE = 'NULL';
+
+ALTER TABLE MULTI_AGE_COMPOSITE_UNPIVOTED
+ALTER COLUMN COMPOSITE_VALUE SET DATA TYPE FLOAT;
+
+UPDATE PUBLIC_SCHOOL_NSW_MASTER_DATASET
+SET PHONE = REPLACE(PHONE, ' ', '');
+
+UPDATE PUBLIC_SCHOOL_NSW_MASTER_DATASET 
+SET INDIGENOUS_PCT = NULL 
+WHERE INDIGENOUS_PCT = 'NP';
+
+ALTER TABLE PUBLIC_SCHOOL_NSW_MASTER_DATASET 
+ALTER COLUMN INDIGENOUS_PCT SET DATA TYPE DOUBLE;
+
+UPDATE PUBLIC_SCHOOL_NSW_MASTER_DATASET 
+SET LBOTE_PCT = NULL 
+WHERE LBOTE_PCT = 'NP';
+
+ALTER TABLE PUBLIC_SCHOOL_NSW_MASTER_DATASET 
+ALTER COLUMN LBOTE_PCT SET DATA TYPE DOUBLE;
+
+UPDATE PUBLIC_SCHOOL_NSW_MASTER_DATASET
 SET DATE_1ST_TEACHER = 
 CASE 
-    WHEN POSITION('/' IN Date_1st_teacher) > 0 THEN -- if the date contains a slash, it's in the DD/MM/YYYY format
+    WHEN POSITION('/' IN DATE_1ST_TEACHER) > 0 THEN -- IF THE DATE CONTAINS A SLASH, IT'S IN THE DD/MM/YYYY FORMAT
         SUBSTR(DATE_1ST_TEACHER, 7, 4) || '-' || 
-        SUBSTR(DATE_1ST_TEACHER, 4, 2) || '-' || l
+        SUBSTR(DATE_1ST_TEACHER, 4, 2) || '-' || L
         SUBSTR(DATE_1ST_TEACHER, 1, 2)
     ELSE 
         DATE_1ST_TEACHER 
 END;
 
-UPDATE public_school_nsw_master_dataset
-SET Date_1st_teacher = CAST(Date_1st_teacher AS DATE);
+UPDATE PUBLIC_SCHOOL_NSW_MASTER_DATASET
+SET DATE_1ST_TEACHER = CAST(DATE_1ST_TEACHER AS DATE);
 
-ALTER TABLE student_attendance_unpivoted
-ALTER COLUMN Year SET DATA TYPE INT;
 
 UPDATE student_attendance_unpivoted 
-SET Attendance_pct = NULL 
-WHERE Attendance_pct IN ('sp', 'na');
-
-
+SET ATTENDANCE_PCT = NULL 
+WHERE ATTENDANCE_PCT IN ('SP');
+ALTER TABLE STUDENT_ATTENDANCE_UNPIVOTED
+ALTER COLUMN YEAR SET DATA TYPE INT;
 ALTER TABLE student_attendance_unpivoted
-ALTER COLUMN Attendance_pct SET DATA TYPE DOUBLE;
+ALTER COLUMN ATTENDANCE_PCT SET DATA TYPE DOUBLE;
 
-SELECT COUNT(*) FROM school_characteristics;
-
-UPDATE multi_age_composite_unpivoted
-SET Composite_Value = ROUND(Composite_Value, 2);
-
-ALTER TABLE multi_age_composite_unpivoted
-RENAME COLUMN School_code TO School_Code;
+SELECT * from student_attendance_unpivoted where ATTENDANCE_PCT ='SP';
 
 
-ALTER TABLE public_school_nsw_master_dataset
-RENAME COLUMN School_code TO School_Code;
+ALTER TABLE STUDENT_ATTENDANCE_UNPIVOTED
+ALTER COLUMN ATTENDANCE_PCT SET DATA TYPE DOUBLE;
 
-DROP TABLE multi_age_composite_unpivoted;
+SELECT COUNT(*) FROM SCHOOL_CHARACTERISTICS;
+
+UPDATE MULTI_AGE_COMPOSITE_UNPIVOTED
+SET COMPOSITE_VALUE = ROUND(COMPOSITE_VALUE, 2);
+
+ALTER TABLE MULTI_AGE_COMPOSITE_UNPIVOTED
+RENAME COLUMN SCHOOL_CODE TO SCHOOL_CODE;
+
+ALTER TABLE PUBLIC_SCHOOL_NSW_MASTER_DATASET
+RENAME COLUMN SCHOOL_CODE TO SCHOOL_CODE;
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
