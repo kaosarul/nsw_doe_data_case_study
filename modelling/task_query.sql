8a469fcd33ad6b6071dd34ace7c4da5ac9614074
Example User <eUser@example.com>
2023-08-18 15:22:38 +0000

    missing value imputation at student_attendance_unpivoted

--- modelling/task_query.sql
+++ modelling/task_query.sql
@@ -1,29 +1,3 @@
-We would like you to develop a PhD research proposal for the title: 
-
-"Deep Transformation of Tabular Categorical Data for Enhanced Predictive Analytics in Health Informatics". 
-
-Please find a tentative summary of the project as given below:
-
-This PhD project will delve into the realm of intelligent tabular data transformation with a focus on categorical data, aiming to enhance the accuracy of predictive analytics within the domain of health informatics. By developing advanced deep learning-based techniques for handling categorical data, the research will address a critical challenge in data preprocessing that significantly influences the quality of predictive models in healthcare. The study seeks to create novel methodologies that automate the transformation process, ensuring the efficient conversion of categorical variables into suitable formats for predictive analysis. Through rigorous experimentation and validation, this research promises to contribute substantially to the improvement of predictive analytics in health informatics, ultimately fostering more precise and reliable decision-making in the medical field.
-
-Please find a sample research proposal attached to this email. Part-time PhD may take 3-6 years. You can pick/propose your timeline accordingly.
-
-I am also attaching two research papers to start the literature review for you.
-
-Tuition fee will be waived under RTP, and we will seek the approval of it for you. We will also seek for industry collaboration for your PhD project.
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 CREATE TABLE MULTI_AGE_COMPOSITE_DATASET AS SELECT * FROM READ_CSV_AUTO('./DATA/MULTI_AGE_COMPOSITE_CLASSES.CSV');
 
@@ -106,6 +80,8 @@ RENAME COLUMN SCHOOL_CODE TO SCHOOL_CODE;
 
 ALTER TABLE PUBLIC_SCHOOL_NSW_MASTER_DATASET
 RENAME COLUMN SCHOOL_CODE TO SCHOOL_CODE;
+
+DROP TABLE student_attendance_data;
 
 
 
